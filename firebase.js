9a06300682d787b4db43159ab8c6b3570e056963
om064750 <>
2021-08-26 19:54:38 +0530

    commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDXL-UBB-C6l9s7_A5yNf6p82iCmcplhfo",
+    authDomain: "aitc-silchar.firebaseapp.com",
+    databaseURL: "https://aitc-silchar-default-rtdb.firebaseio.com",
+    projectId: "aitc-silchar",
+    storageBucket: "aitc-silchar.appspot.com",
+    messagingSenderId: "171795590201",
+    appId: "1:171795590201:web:62e3c18c92e96ae3fab1e8"
+  };
